7d230935a5281e5ab06aed2c56c34f713cbd818c
Gibson Fahnestock <gibfahn@gmail.com>
2017-12-07 10:43:07 +0000

    06: Working solution for both parts

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -2,19 +2,20 @@ fn main() {
     println!("Hello, world!");
 }
 
+pub enum Problem {
+    First,
+    Second,
+}
+
 /// Takes a list of memory banks, and tries to redistribute them as much as possible. Returns
-/// the number of redistributions until it starts to cycle.
-pub fn redist_count(s: &str) -> usize {
+/// the number of redistributions until it starts to cycle if Problem::First passed, or the
+/// number of iterations since the loop if Problem::Second passed.
+pub fn redist_count(s: &str, p: Problem) -> usize {
     let mut banks: Vec<usize> = s.split_whitespace().map(|word| word.parse().unwrap()).collect();
     let bank_len = banks.len();
-    let mut bank_history: Vec<Vec<usize>> = Vec::new();
-    let mut iterations = 0;
+    let mut bank_history: Vec<Vec<usize>> = Vec::with_capacity(100);
     while ! bank_history.iter().any(|bank| bank.iter().zip(banks.iter()).all(|(a,b)| a == b)) {
         bank_history.push(banks.clone());
-        iterations += 1;
-        // println!("Banks: {:?}", banks);
-        // println!("Bank history: {:?}", bank_history);
-        // TODO(gib): Fix unnecessary clone (wish we had NLL).
         let mut counter;
         let mut max;
         {
@@ -31,17 +32,39 @@ pub fn redist_count(s: &str) -> usize {
             max -= 1;
         }
     }
-    iterations
+
+    match p {
+        Problem::First => bank_history.len(),
+        Problem::Second => {
+            // TODO(gib): We could avoid this calculation by storing the result from the while
+            // loop test.
+            let (i, _) = bank_history.iter().enumerate()
+                .filter(|&(_, bank)| bank.iter().zip(banks.iter()).all(|(a,b)| a == b)).nth(0).unwrap();
+            bank_history.len() - i
+        }
+    }
 }
 
 #[test]
 fn example_1() {
     let input = "0 2 7 0";
-    assert_eq!(redist_count(input), 5);
+    assert_eq!(redist_count(input, Problem::First), 5);
 }
 
 #[test]
 fn problem_1() {
     let input = include_str!("../input.txt");
-    assert_eq!(redist_count(input), 7864);
+    assert_eq!(redist_count(input, Problem::First), 7864);
+}
+
+#[test]
+fn example_2() {
+    let input = "0 2 7 0";
+    assert_eq!(redist_count(input, Problem::Second), 4);
+}
+
+#[test]
+fn problem_2() {
+    let input = include_str!("../input.txt");
+    assert_eq!(redist_count(input, Problem::Second), 1695);
 }
