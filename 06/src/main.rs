2555bd5fce3c6de097e518bf67ca1684eaccf837
Gibson Fahnestock <gibfahn@gmail.com>
2017-12-07 10:22:25 +0000

    06: First half of day six

---
+++ 06/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/// Takes a list of memory banks, and tries to redistribute them as much as possible. Returns
+/// the number of redistributions until it starts to cycle.
+pub fn redist_count(s: &str) -> usize {
+    let mut banks: Vec<usize> = s.split_whitespace().map(|word| word.parse().unwrap()).collect();
+    let bank_len = banks.len();
+    let mut bank_history: Vec<Vec<usize>> = Vec::new();
+    let mut iterations = 0;
+    while ! bank_history.iter().any(|bank| bank.iter().zip(banks.iter()).all(|(a,b)| a == b)) {
+        bank_history.push(banks.clone());
+        iterations += 1;
+        // println!("Banks: {:?}", banks);
+        // println!("Bank history: {:?}", bank_history);
+        // TODO(gib): Fix unnecessary clone (wish we had NLL).
+        let (mut counter, mut max) = banks.clone().into_iter()
+            .enumerate().max_by_key(|&(a, b)| (b, bank_len - a)).unwrap();
+        banks[counter] = 0;
+        while max > 0 {
+            counter = (counter + 1) % bank_len;
+            banks[counter] += 1;
+            max -= 1;
+        }
+    }
+    iterations
+}
+
+#[test]
+fn example_1() {
+    let input = "0 2 7 0";
+    assert_eq!(redist_count(input), 5);
+}
+
+#[test]
+fn problem_1() {
+    let input = include_str!("../input.txt");
+    assert_eq!(redist_count(input), 7864);
+}
