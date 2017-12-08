bd883148c29a282e3da8207c9588559a8337bfe1
Gibson Fahnestock <gibfahn@gmail.com>
2017-12-08 14:03:49 +0000

    08: Run cargo clippy on results and improve

--- 08/src/lib.rs
+++ 08/src/lib.rs
@@ -6,19 +6,15 @@ pub enum Problem {
 }
 
 /// Parses a list of assembly instructions and returns the largest value stored in a register.
-pub fn get_largest(s: &str, p: Problem) -> isize {
+pub fn get_largest(s: &str, p: &Problem) -> isize {
     let mut registers: HashMap<String, isize> = HashMap::new();
     let mut max_val = 0;
     for line in s.lines() {
-        // println!("===> Line: {}", line);
         let words: Vec<&str> = line.split_whitespace().collect();
-        // println!("words: {:?}", words);
         assert_eq!(words[3], "if");
-        // println!("max_val before: {}", max_val);
-        if {
+        let condition = { // If the condition doesn't match we shouldn't perform the operation.
             let lhs: isize = words[4].parse::<isize>().ok().unwrap_or(*registers.entry(words[4].to_owned()).or_insert(0));
             let rhs: isize = words[6].parse::<isize>().ok().unwrap_or(*registers.entry(words[6].to_owned()).or_insert(0));
-            // println!("{} {} {} => {}", lhs, rhs, words[5], lhs >= rhs);
             match words[5] {
                 ">" => lhs > rhs,
                 "<" => lhs < rhs,
@@ -28,9 +24,9 @@ pub fn get_largest(s: &str, p: Problem) -> isize {
                 "!=" => lhs != rhs,
                 x => panic!(format!("Unknown operator in position 5: {}", x)),
             }
-        } {
+        };
+        if condition {
             let register = registers.entry(String::from(words[0])).or_insert(0);
-            // println!("Register: {}", register);
             let number: isize = words[2].parse().unwrap();
             match words[1] {
                 "inc" => *register += number,
@@ -39,10 +35,8 @@ pub fn get_largest(s: &str, p: Problem) -> isize {
             };
             max_val = std::cmp::max(*register, max_val);
         }
-        // println!("max_val before: {}", max_val);
     }
-    // println!("Registers: {:#?}", registers);
-    match p {
+    match *p {
         Problem::First => *registers.iter().max_by_key(|&(_, val)| val).unwrap().1,
         Problem::Second => max_val,
     }
@@ -55,18 +49,18 @@ b inc 5 if a > 1
 a inc 1 if b < 5
 c dec -10 if a >= 1
 c inc -20 if c == 10";
-    assert_eq!(get_largest(input.trim(), Problem::First), 1);
-    assert_eq!(get_largest(input.trim(), Problem::Second), 10);
+    assert_eq!(get_largest(input.trim(), &Problem::First), 1);
+    assert_eq!(get_largest(input.trim(), &Problem::Second), 10);
 }
 
 #[test]
 fn problem_1() {
     let input = include_str!("../input.txt");
-    assert_eq!(get_largest(input.trim(), Problem::First), 3612);
+    assert_eq!(get_largest(input.trim(), &Problem::First), 3612);
 }
 
 #[test]
 fn problem_2() {
     let input = include_str!("../input.txt");
-    assert_eq!(get_largest(input.trim(), Problem::Second), 3818);
+    assert_eq!(get_largest(input.trim(), &Problem::Second), 3818);
 }
