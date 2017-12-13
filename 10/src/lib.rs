80732ddf7972358283c65b0fe0bea2fc5eda071e
Gibson Fahnestock <gibfahn@gmail.com>
2017-12-13 22:47:30 +0000

    10: slightly condense

--- 10/src/lib.rs
+++ 10/src/lib.rs
@@ -29,28 +29,21 @@ impl KnotHash {
         }
     }
 
-    pub fn knot_hash(&mut self) {
-        // println!("\n  list_size: {}", list_size);
+    fn knot_hash(&mut self) {
         for &length in &self.lengths {
-            // println!("\nlist: {:?}", list);
-            // println!("position: {}, length: {}, skip_size: {}", position, length, skip_size);
             if length > 1 && length <= self.list_size {
                 let mut start_pos = self.position;
                 let mut end_pos = (self.position + length - 1) % self.list_size;
                 let mut swap_length = length;
                 while swap_length > 1 {
-                    // println!("start_pos: {}, end_pos: {}, swap_length: {}", start_pos, end_pos, swap_length);
                     self.list.swap(start_pos, end_pos);
                     start_pos = (start_pos + 1) % self.list_size;
                     end_pos = if end_pos == 0 { self.list_size - 1 } else { end_pos - 1 };
                     swap_length -= 2;
                 }
-            } else {
-                // println!("SKIPPING LENGTH: length: {}, skip_size: {}", length, skip_size);
             }
             self.position = (self.position + self.skip_size + length) % self.list_size;
             self.skip_size += 1;
-            // println!("End list: {:?}", list);
         }
     }
     pub fn simple_product(&mut self) -> usize {
@@ -59,21 +52,17 @@ impl KnotHash {
     }
 
     pub fn complex_product(&mut self) -> String {
-        for _ in 0..64 {
-            self.knot_hash();
-        }
+        for _ in 0..64 { self.knot_hash(); }
         self.list.chunks(16).map(|chunk|
             format!("{:02x}", chunk.iter().fold(0, |acc, n| acc ^ n))
         ).collect()
     }
-
 }
-
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn example_1_1() {
         assert_eq!(KnotHash::from_simple("3,4,1,5", 5).simple_product(), 12);
@@ -91,29 +80,15 @@ mod tests {
 
     #[test]
     fn example_2_1() {
-        assert_eq!(
-            KnotHash::from_complex("1,2,3", 256).complex_product(),
-            String::from("3efbe78a8d82f29979031a4aa0b16a9d")
-           );
-        assert_eq!(
-            KnotHash::from_complex("AoC 2017", 256).complex_product(),
-            String::from("33efeb34ea91902bb2f59c9920caa6cd")
-           );
-        assert_eq!(
-            KnotHash::from_complex("1,2,3", 256).complex_product(),
-            String::from("3efbe78a8d82f29979031a4aa0b16a9d")
-           );
-        assert_eq!(
-            KnotHash::from_complex("1,2,4", 256).complex_product(),
-            String::from("63960835bcdc130f0b66d7ff4f6a5a8e")
-           );
+        assert_eq!( KnotHash::from_complex("1,2,3", 256).complex_product(), String::from("3efbe78a8d82f29979031a4aa0b16a9d"));
+        assert_eq!( KnotHash::from_complex("AoC 2017", 256).complex_product(), String::from("33efeb34ea91902bb2f59c9920caa6cd"));
+        assert_eq!( KnotHash::from_complex("1,2,3", 256).complex_product(), String::from("3efbe78a8d82f29979031a4aa0b16a9d"));
+        assert_eq!( KnotHash::from_complex("1,2,4", 256).complex_product(), String::from("63960835bcdc130f0b66d7ff4f6a5a8e"));
     }
 
     #[test]
     fn problem_2() {
-        assert_eq!(
-            KnotHash::from_complex(include_str!("../input.txt"), 256).complex_product(), 
-            String::from("70b856a24d586194331398c7fcfa0aaf")
-            );
+        let input = include_str!("../input.txt");
+        assert_eq!( KnotHash::from_complex(input, 256).complex_product(), String::from("70b856a24d586194331398c7fcfa0aaf"));
     }
 }
