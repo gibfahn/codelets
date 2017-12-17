124dba58b336fdad795627c0a090be2d15ac20ec
Gibson Fahnestock <gibfahn@gmail.com>
2017-12-17 09:22:19 +0000

    redblue: Add readme and tweak solution

---
+++ redblue/README.md
@@ -0,0 +1,5 @@
+# RedBlue
+
+Given a pattern string like "abba" and an input string like "redbluebluered",
+return true if and only if there's a one to one mapping of letters in the
+pattern to substrings of the input.
--- redblue/src/lib.rs
+++ redblue/src/lib.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
-/// Checks whether a given pattern (`pattern`) matches a string (`s`). The pattern matches if
-/// each letter in the pattern makes up a word in the string. For example `abba` matches
+/// Checks whether a given pattern (`pattern`) matches a input (`s`). The pattern matches if
+/// each letter in the pattern makes up a word in the input. For example `abba` matches
 /// `redblueredblue` when a=red, b=blue.
-pub fn matches(pattern: &str, string: &str) -> bool {
+pub fn matches(pattern: &str, input: &str) -> bool {
     let mut map = HashMap::new(); // Map of pattern char -> Word.
     let mut repeated_chars = false;
     for p in pattern.chars() {
@@ -14,29 +14,29 @@ pub fn matches(pattern: &str, string: &str) -> bool {
         }
     }
     if ! repeated_chars { return true; }
-    check_string(pattern, string, map)
+    check_string(pattern, input, map)
 }
 
-fn check_string(pattern: &str, string: &str, map: HashMap<char, String>) -> bool {
-    if pattern.is_empty() || string.is_empty() {
-        return pattern.is_empty() && string.is_empty();
+fn check_string(pattern: &str, input: &str, map: HashMap<char, String>) -> bool {
+    if pattern.is_empty() || input.is_empty() {
+        return pattern.is_empty() && input.is_empty();
     }
     let p = pattern.chars().nth(0).unwrap(); // Next char in pattern.
     let p_string = map.get(&p).unwrap(); // The word the map thinks p maps to.
     if p_string.is_empty() { // Have not seen pattern before.
-        for i in 1..string.len() {
+        for i in 1..input.len() {
             let mut new_map = map.clone();
-            new_map.insert(p, string[0..i].to_string());
+            new_map.insert(p, input[0..i].to_string());
             // Try all possible matches for p.
-            if check_string(&pattern[1..], &string[i..], new_map) {
+            if check_string(&pattern[1..], &input[i..], new_map) {
                 return true;
             }
         }
         false
     } else { // Have seen pattern before.
         let p_string_len = p_string.len();
-        if p_string_len <= string.len() && p_string == &string[0..p_string_len] { // Pattern matches previously seen pattern.
-            check_string(&pattern[1..], &string[p_string_len..], map.clone()) // Try rest of string.
+        if p_string_len <= input.len() && p_string == &input[0..p_string_len] { // Pattern matches previously seen pattern.
+            check_string(&pattern[1..], &input[p_string_len..], map.clone()) // Try rest of string.
         } else { // Pattern doesn't match.
             false
         }
