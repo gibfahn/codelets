f7ee87cdc87285d36882f34a51938672bb715be4
Gibson Fahnestock <gibfahn@gmail.com>
2018-12-03 12:03:26 +0000

    03: messy full solution
    Solution works, but could do with some cleanup.

--- advent_2018/03/src/lib.rs
+++ advent_2018/03/src/lib.rs
@@ -1,7 +1,9 @@
 #![feature(external_doc)]
 #![doc(include = "../Question.md")]
 
-use failure::{bail, ensure, Error};
+use std::collections::{HashSet, HashMap};
+
+use failure::{bail, Error};
 use std::str::FromStr;
 
 const INPUT: &str = include_str!("../input");
@@ -25,23 +27,6 @@ struct Claim {
     id: u32,
     top_left: Point,
     bottom_right: Point,
-}
-
-impl Claim {
-    fn overlaps(&self, other: &Self) -> bool {
-
-        // If one rectangle is on left side of other
-        if (self.top_left.x > other.bottom_right.x || self.bottom_right.x > other.top_left.x) {
-            return false;
-        }
-
-        // If one rectangle is above other
-        if (self.top_left.y < other.bottom_right.y || self.bottom_right.y < other.top_left.y) {
-            return false;
-        }
-
-        return true;
-    }
 }
 
 impl FromStr for Claim {
@@ -73,45 +58,6 @@ impl FromStr for Claim {
             top_left: Point { x: left_offset, y: top_offset },
             bottom_right: Point { x: left_offset + width, y: top_offset + height },
         })
-
-        /*
-        let words: Vec<&str> = s.split_whitespace().collect();
-        if words.len() != 4 {
-            bail!("Couldn't parse into Claim: {}", s)
-        }
-
-        let mut chars = words[0].chars();
-        let mut chunk: String = chars.by_ref().take(1).collect();
-        ensure!(chunk == "#",
-                "Expected first char to be a '#', instead it was a {:?}",
-                chunk);
-        let id = chars.collect::<String>().parse::<u32>()?;
-
-        ensure!(words[1] == "@",
-                "Expected second word to be '@', instead it was {:?}",
-                words[1]);
-
-        chars = words[2].chars();
-        chunk = chars.by_ref().take_while(|c| *c != ',').collect();
-        let left_offset = chunk.parse::<u32>()?;
-        chunk = chars.by_ref().take_while(|c| *c != ':').collect();
-        let top_offset = chunk.parse::<u32>()?;
-
-        let trailing_chars = chars.collect::<String>();
-        ensure!(trailing_chars == "",
-                "Third word has trailing characters: {:?}",
-                trailing_chars);
-
-        chars = words[3].chars();
-        chunk = chars.by_ref().take_while(|c| *c != 'x').collect();
-        let width = chunk.parse::<u32>()?;
-        let height = chars.collect::<String>().parse::<u32>()?;
-
-        Ok(Claim {id,
-            top_left: Point { x: left_offset, y: top_offset },
-            bottom_right: Point { x: left_offset + width, y: top_offset + height },
-        })
-        */
     }
 }
 
@@ -127,30 +73,27 @@ fn overlaps(input: &str) -> Result<usize, Error> {
     }
     let claims: Vec<_> = results.into_iter().map(Result::unwrap).collect();
 
-    let max_x = claims.iter().max_by_key(|c| c.bottom_right.x).unwrap().bottom_right.x + 1;
-    let max_y = claims.iter().max_by_key(|c| c.bottom_right.y).unwrap().bottom_right.y + 1;
-
-    let mut cloth = vec![vec![0_u8; max_x as usize]; max_y as usize];
+    let mut claim_map = HashMap::new();
+    let mut claim_names = HashMap::new();
+    let mut overlapping = HashSet::new();
+    let mut all = HashSet::new();
 
     for claim in claims {
-        // println!("{:?}", claim);
-        for y in claim.top_left.y..claim.bottom_right.y {
-            for x in claim.top_left.x..claim.bottom_right.x {
-                // println!("{}, {}", y, x);
-                cloth[y as usize][x as usize] = cloth[y as usize][x as usize].saturating_add(1);
+        all.insert(claim.id);
+        for x in claim.top_left.x..claim.bottom_right.x {
+            for y in claim.top_left.y..claim.bottom_right.y {
+                *claim_map.entry((x,y)).or_insert(0) += 1;
+                if !claim_names.contains_key(&(x,y)) {
+                    claim_names.insert((x,y), claim.id);
+                } else {
+                    overlapping.insert(claim_names[&(x,y)]);
+                    overlapping.insert(claim.id);
+                }
             }
         }
     }
 
-    // println!("\n");
-    // for row in &cloth {
-    //     for val in row {
-    //         print!("{}", val);
-    //     }
-    //     println!("");
-    // }
-    // println!("\n\n Cloth:\n{:?}", cloth);
-    Ok(cloth.iter().flat_map(|row| row.iter()).filter(|count| **count > 1).count())
+    Ok(claim_map.values().filter(|v| **v > 1).count())
 }
 
 /// Return the ID of the claim with no overlap with any others.
@@ -164,28 +107,27 @@ fn no_overlap(input: &str) -> Result<u32, Error> {
     }
     let claims: Vec<_> = results.into_iter().map(Result::unwrap).collect();
 
-    let max_x = claims.iter().max_by_key(|c| c.bottom_right.x).unwrap().bottom_right.x + 1;
-    let max_y = claims.iter().max_by_key(|c| c.bottom_right.y).unwrap().bottom_right.y + 1;
+    let mut claim_map = HashMap::new();
+    let mut claim_names = HashMap::new();
+    let mut overlapping = HashSet::new();
+    let mut all = HashSet::new();
 
-    let mut output = Vec::new();
-    for (n, claim) in claims.iter().enumerate() {
-        let mut overlaps = false;
-        for (m, other_claim) in claims.iter().enumerate() {
-            if n == m {
-                continue;
+    for claim in claims {
+        all.insert(claim.id);
+        for x in claim.top_left.x..claim.bottom_right.x {
+            for y in claim.top_left.y..claim.bottom_right.y {
+                *claim_map.entry((x,y)).or_insert(0) += 1;
+                if !claim_names.contains_key(&(x,y)) {
+                    claim_names.insert((x,y), claim.id);
+                } else {
+                    overlapping.insert(claim_names[&(x,y)]);
+                    overlapping.insert(claim.id);
+                }
             }
-            if claim.overlaps(&other_claim) {
-                overlaps = true;
-            }
-        }
-        if overlaps == false {
-            output.push(claim.id);
         }
     }
-    ensure!(output.len() == 1,
-            "Wrong number of non-overlapping claims found, expected 1, found {:?}",
-            output);
-    Ok(output[0])
+
+    Ok(*all.difference(&overlapping).next().unwrap())
 }
 
 #[cfg(test)]
