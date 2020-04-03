884bbf8b7a9a25266fce018f129fc65a88767388
Gibson Fahnestock <gibfahn@gmail.com>
2020-04-03 18:48:59 +0100

    Advent of Code 2019 days 4-9

---
+++ advent_2019/src/day_04/mod.rs
@@ -0,0 +1,3 @@
+pub fn answer() -> (String, String) {
+    (String::new(), String::new())
+}
--- advent_2019/src/main.rs
+++ advent_2019/src/main.rs
@@ -1,12 +1,17 @@
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
 
 fn main() {
     println!("\nAdvent of Code 2019 Answers:\n");
 
-    let day_fns: Vec<(fn() -> (String, String))> =
-        vec![day_01::answer, day_02::answer, day_03::answer];
+    let day_fns: Vec<(fn() -> (String, String))> = vec![
+        day_01::answer,
+        day_02::answer,
+        day_03::answer,
+        day_04::answer,
+    ];
 
     for (n, answer) in day_fns.iter().enumerate() {
         let (first, second) = answer();
