94be3f6e1cdd2b5dcb468bb85b1a0911133756de
Gibson Fahnestock <gibfahn@gmail.com>
2024-12-09 13:17:46 +0000

    fix(informatics): use swift testing framework

--- informatics_olympiad/2024/Tests/IntegerStringsTests/IntegerStringsTests.swift
+++ informatics_olympiad/2024/Tests/IntegerStringsTests/IntegerStringsTests.swift
@@ -1,38 +1,29 @@
-import XCTest
-import IntegerStrings
+// Run with `swift test -c release` (last test is slowwww so needs release mode)
 
-/// Can't compare Tuples by default, so need a custom comparator.
-/// <https://stackoverflow.com/a/38192766>
-private func assertTupleEqual(_ expected: (_: Int, _: Int), _ actual: (_: Int, _: Int), file: StaticString = #file, line: UInt = #line) {
-    if expected != actual {
-        XCTFail("Expected \(expected) but was \(actual)", file: file, line: line)
-    }
+import IntegerStrings
+import Testing
+import XCTest
+
+@Test func testNthDigit() throws {
+    #expect(1 == nthDigit(n: 999, i: 11))
 }
 
-final class IntegerStringTests: XCTestCase {
-    func testNthDigit() throws {
-        let expected = 1
-        let actual = nthDigit(n: 999, i: 11)
-        XCTAssertEqual(expected, actual)
-    }
+@Test func testCountFives() throws {
+    #expect(11 == countFives(n: 1, i: 101))
+}
 
-    func testCountFives() throws {
-        XCTAssertEqual(11, countFives(n: 1, i: 101))
-    }
+@Test func testFindSubstringNaive() throws {
+    #expect((1, 9) == findSubstringNaive(n: 1, substring: "123456789"))
+    #expect((223, 227) == findSubstringNaive(n: 1, substring: "11111"))
 
-    func testFindSubstringNaive() throws {
-        assertTupleEqual((1, 9), findSubstringNaive(n: 1, substring: "123456789"))
-        assertTupleEqual((223, 227), findSubstringNaive(n: 1, substring: "11111"))
+}
 
-    }
+@Test func testFindSubstringFaster() throws {
+    #expect((1, 9) == findSubstringFaster(n: 1, substring: "123456789"))
+    #expect((223, 227) == findSubstringFaster(n: 1, substring: "11111"))
+}
 
-    func testFindSubstringFaster() throws {
-        assertTupleEqual((1, 9), findSubstringFaster(n: 1, substring: "123456789"))
-        assertTupleEqual((223, 227), findSubstringFaster(n: 1, substring: "11111"))
-    }
-
-    func testFindSubstringFasterBig() throws {
-        assertTupleEqual((1677777779, 1677777787), findSubstringFaster(n: 1, substring: "987654321"))
-    }
-
+@Test func testFindSubstringFasterBig() throws {
+    #expect(
+        (1_677_777_779, 1_677_777_787) == findSubstringFaster(n: 1, substring: "987654321"))
 }
